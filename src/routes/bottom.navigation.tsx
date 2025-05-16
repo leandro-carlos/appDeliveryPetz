43cdd74390d693d68f8d74a2cf423b3204442b93
Leandro.carlos <leandro.carlosleo2015@gmail.com>
2025-05-16 17:50:22 -0300

    feat: estiliza bottom navigation e remove comentário desnecessário
    Implementa a estilização da bottom navigation, adicionando cores e ícones, e remove um comentário desnecessário no componente RootNavigation.
    
    As seguintes mudanças foram realizadas:
    
    - Adicionado ícones da biblioteca `phosphor-react-native` para os tabs da bottom navigation.
    - Adicionado cores do tema para os ícones ativos e inativos da bottom navigation.

--- src/routes/bottom.navigation.tsx
+++ src/routes/bottom.navigation.tsx
@@ -1,16 +1,64 @@
 import Historic from "@/pages/Historic";
 import Home from "@/pages/Home";
 import Profile from "@/pages/Profile";
+import theme from "@/theme";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { House, List, Notepad } from "phosphor-react-native";
 
 const Tab = createBottomTabNavigator();
 
 function Bottom() {
   return (
-    <Tab.Navigator initialRouteName="Home" id={undefined}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Historic" component={Historic} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.grayMedium,
+        tabBarStyle: { backgroundColor: theme.colors.background },
+      }}
+      initialRouteName="Home"
+      id={undefined}
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <House
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.grayMedium}
+            />
+          ),
+          tabBarLabel: "Home",
+          tabBarInactiveTintColor: theme.colors.grayMedium,
+        }}
+        name="Home"
+        component={Home}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Notepad
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.grayMedium}
+            />
+          ),
+          tabBarLabel: "Historico",
+        }}
+        name="Historic"
+        component={Historic}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <List
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.grayMedium}
+            />
+          ),
+          tabBarLabel: "Menu",
+        }}
+        name="Profile"
+        component={Profile}
+      />
     </Tab.Navigator>
   );
 }
--- src/routes/root.navigation.tsx
+++ src/routes/root.navigation.tsx
@@ -4,7 +4,7 @@ import { Bottom } from "./bottom.navigation";
 import { useAuthContext } from "@/context/AuthContext";
 
 export default function RootNavigation() {
-  const { isAuthenticated } = useAuthContext(); // ✅ Correto, pois já está dentro do AuthProvider
+  const { isAuthenticated } = useAuthContext();
 
   return (
     <NavigationContainer>
